4a546b27341fe7e70a393599a35ddd70635b698e
Ji Sungbin <ji@sungb.in>
2021-07-05 04:39:21 +0900

    Update 병든나이트.kt

--- 백준/병든나이트.kt
+++ 백준/병든나이트.kt
@@ -3,13 +3,11 @@ import kotlin.math.min
 fun main() {
     val (n, m) = readLine()!!.split(" ").map { it.toInt() }
     println(
-        if (n == 1) {
-            1
-        } else if (n == 2) {
-            min(4, ((m - 1) / 2) + 1)
-        } else {
-            if (m < 7) min(4, m)
-            else 3 + (m - 5)
+        when {
+            n == 1 -> 1
+            n == 2 -> min(4, ((m - 1) / 2) + 1)
+            m < 7 -> min(4, m)
+            else -> 3 + (m - 5)
         }
     )
 }
