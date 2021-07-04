17e583dc4b4d811a2dfb34d00f0bf04d97bd234b
Ji Sungbin <ji@sungb.in>
2021-07-05 00:45:35 +0900

    Create 1이될때까지.kt

---
+++ 이것이코딩테스트다/1이될때까지.kt
@@ -0,0 +1,16 @@
+  
+fun main() {
+    var (n, k) = readLine()!!.split(" ").map { it.toInt() }
+    var result = 0
+    while (n > 1) {
+        if (n % k == 0) {
+            n /= k
+            result += 1
+        } else {
+            n += 1
+            result += 1
+        }
+    }
+
+    println(result)
+}
