9bbdb211cf754d17f1836115715f1f0013a49af6
Ji Sungbin <ji@sungb.in>
2021-12-06 14:24:48 +0900

    Create 듣보잡.kt

---
+++ 백준/듣보잡.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+// a.intersect(b).sorted() 로 중복되는 요소 가져올 수 있음
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val peopleCount = br.readLine()!!.split(" ").sumOf { it.toInt() }
+    val names = mutableListOf<String>()
+
+    repeat(peopleCount) {
+        names.add(br.readLine()!!)
+    }
+
+    val noPlacedPeoples = names.groupBy { it }.filter { it.value.size > 1 }.values.flatten().distinct().sorted()
+
+    println(noPlacedPeoples.size)
+    noPlacedPeoples.forEach(::println)
+
+    br.close()
+}
