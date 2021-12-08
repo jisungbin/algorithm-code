4221a203ff6aa6e4189a982d0a9f6cc5558c406f
Ji Sungbin <ji@sungb.in>
2021-12-08 20:44:01 +0900

    Update 데이터 체커.kt

--- 백준/데이터 체커.kt
+++ 백준/데이터 체커.kt
@@ -12,21 +12,30 @@ fun main() {
         val (x, r) = br.readLine()!!.split(" ").map { it.toInt() }
         x - r to x + r
     }.sortedWith { o1, o2 ->
+        // 같지 않는 값 찾아서 오름차순 정렬
         if (o1.first != o2.first) o1.first - o2.first
         else o1.second - o2.second
     }.toMutableList()
 
     var newCircle: Pair<Int, Int>
-    var lastCircle = circles[0]
+    var lastCircleEndPoint = circles[0].second
     var isCircleNonOverlap = "YES"
 
     for (i in 1 until circles.size) {
         newCircle = circles[i]
-        if (lastCircle.second >= newCircle.first && lastCircle.second <= newCircle.second) {
+        // 마지막으로 그린 원의 끝 점이 새로 그릴 원의 시작점보다 밖에 있거나 같고,
+        // 마지막으로 그린 원의 끝 점이 새로 그릴 원의 끝 점보다 작거나 같으면
+        // (새로 그릴 원의 끝 점이 마지막으로 그린 원의 끝 점부터 크거나 같으면)
+        // (만약 마지막으로 그린 원의 끝 점이 새로 그릴 원의 끝 점보다 크다면, 새로 그릴 원이 마지막으로 그린 원 안에 들어가서 겹점이 안 생김)
+        // 새로 그릴 원의 시작 점이 마지막으로 그린 원 안에서 시작하고, 새로 그릴 원의 끝 점이 마지막으로 그린 원 바깥쪽에 그려지므로 겹점이 생김
+
+        // 원이 시작 좌표로 오름차순으로 정렬이 돼서 1~~ 순서로 점점 시작 점이 커지므로,
+        // 원 시작점 비교를 마지막으로 그린 원의 끝 점과 새로 그릴 원의 시작 점으로 비교를 해야 함
+        if (lastCircleEndPoint >= newCircle.first && lastCircleEndPoint <= newCircle.second) {
             isCircleNonOverlap = "NO"
             break
         }
-        lastCircle = newCircle
+        lastCircleEndPoint = newCircle.second
     }
 
     bw.write(isCircleNonOverlap)
