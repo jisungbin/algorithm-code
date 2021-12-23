b02081d61771cb6aee943c26037518686631325d
Ji Sungbin <ji@sungb.in>
2021-12-23 18:50:22 +0900

    Update 트리의 지름.kt

--- 백준/트리의 지름.kt
+++ 백준/트리의 지름.kt
@@ -3,9 +3,6 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
-// 물을 가지고 있으며, 자식 정점이 있다면 자식 정점 중 하나를 골라 물을 1 준다. 자식 정점이 여러 개라면 동일한 확률로 그 중 하나를 고른다.
-// -> 리프 노드일 때만 물을 가지고 있을 수 있음 (자식 노드가 있다면, 자식 노드에게 계속 물을 넘김)
-
 private data class Node(val key: Int, val weight: Int)
 
 fun main() {
